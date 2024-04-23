6a8bd5ed795de4f9353fe72ed378c2dbd75a20a8
Strolluj-Trolla <marty.pastuszak@gmail.com>
2024-04-23 13:58:45 +0200

    Delete fix

--- trees/BST.py
+++ trees/BST.py
@@ -65,35 +65,61 @@ class BSTnode:
         res+=" \{"+left+"} \{"+right+"}"
         return "\\"+res
     
-    def delete(self, value):
+    def delete(self, value) -> int:
         if self.value==value:
             if self.left==None and self.right==None:
                 self=None
-                return
+                return 1
             if self.left==None:
                 self=self.right
-                return
+                return 3
             if self.right==None:
                 self=self.left
-                return
+                return 2
             if abs(self.left.maxNode())-abs(value<self.right.minNode()-value):
                 value=self.left.maxNode()
                 self.value=value
-                self.left.delete(value)
-                return
+                res=self.left.delete(value)
+                if res==1:
+                    self.left=None
+                if res==2:
+                    self.left=self.left.left
+                if res==3:
+                    self.left=self.left.right
+                return 0
             else:
                 value=self.right.minNode()
                 self.value=value
                 self.right.delete(value)
-                return
+                if res==1:
+                    self.right=None
+                if res==2:
+                    self.right=self.right.left
+                if res==3:
+                    self.right=self.right.right
+                return 0
         else:
             if self.left==None and self.right==None:
-                return
+                return 0
             if value>self.value:
-                self.right.delete(value)
-                return
-            self.left.delete(value)
-            return
+                res=self.right.delete(value)
+                if res==1:
+                    self.right=None
+                if res==2:
+                    self.right=self.right.left
+                if res==3:
+                    self.right=self.right.right
+                return 0
+            if value<self.value:
+                res=self.left.delete(value)
+                if res==1:
+                    self.left=None
+                if res==2:
+                    self.left=self.left.left
+                if res==3:
+                    self.left=self.left.right
+                return 0
+            return 0
             
 inp=input("Podaj liste: ")
 inp=[int(x) for x in inp.strip().split()]
@@ -110,4 +136,10 @@ print("Min "+str(tree.minNode()))
 print("In-order: "+tree.inOrder())
 print("Pre-order: "+tree.preOrder())
 print("Post-order: "+tree.postOrder())
-print(tree.TiKZgenerate())
+#print(tree.TiKZgenerate())
+
+tree.delete(8)
+
+print("In-order: "+tree.inOrder())
+print("Pre-order: "+tree.preOrder())
+print("Post-order: "+tree.postOrder())
