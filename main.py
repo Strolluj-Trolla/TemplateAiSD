0f9e6fc87b87d2a6fbe98cb568ec3e9f55dc277f
Strolluj-Trolla <marty.pastuszak@gmail.com>
2024-06-06 14:53:47 +0200

    Add help

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ def is_number(s):
         return False
     
 def help():
-    print("")
-    print("")
+    print("Available instructions:")
+    print("Help - display this information;")
+    print("Print - print the graph;")
+    print("Hamiltonian - find and print a hamiltonian path, if it exists;")
+    print("Euler - find and print a Euler path, if it exists;")
+    print("Exit - exit the program.")
 
 # Checks if the Python script is being run as the main program (not imported as a module)
 if __name__ == "__main__":
@@ -74,9 +78,9 @@ if __name__ == "__main__":
             print(hamilton.Hcycle(graph, node_count, visited, Visited_vert, current_path))
 
         if response=="Euler":
-            Visited_vert = [False] * node_count
-            visited = 0
-            current_path = []
+            #Visited_vert = [False] * node_count
+            #visited = 0
+            #current_path = []
             tmp = copy.deepcopy(graph)
             print(hamilton.euler(tmp, 0, [], node_count))
 
